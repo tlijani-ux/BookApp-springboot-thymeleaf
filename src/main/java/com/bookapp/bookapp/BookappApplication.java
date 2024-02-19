3785cc30b466ba8c9f1f909161fdbb2e54754053
Seif Tlijani <114825087+tlijani-ux@users.noreply.github.com>
2024-02-19 17:35:51 +0100

    first comm

---
+++ src/main/java/com/bookapp/bookapp/BookappApplication.java
@@ -0,0 +1,15 @@
+package com.bookapp.bookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class BookappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bookapp/bookapp/controllers/BookController.java
@@ -0,0 +1,19 @@
+package com.bookapp.bookapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class BookController {
+	
+	
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+		
+	}
+
+}
